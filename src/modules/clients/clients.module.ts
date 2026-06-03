40f7ea6a1dee83af1b7cd8cb289d9953ef1d304e
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2026-06-03 22:00:32 +0500

    fix: import TelegramModule in ClientsModule to resolve DI error

--- src/modules/clients/clients.module.ts
+++ src/modules/clients/clients.module.ts
@@ -10,9 +10,10 @@ import { ActivityLog } from '../archive/entities/activity-log.entity';
 import { ClientsGateway } from './gateways/clients.gateway';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { User } from '../users/entities/user.entity';
+import { TelegramModule } from '../telegram/telegram.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Client, ClientNote, Payment, Department, ActivityLog, User]), NotificationsModule],
+    imports: [TypeOrmModule.forFeature([Client, ClientNote, Payment, Department, ActivityLog, User]), NotificationsModule, TelegramModule],
     controllers: [ClientsController],
     providers: [ClientsService, ClientsGateway],
     exports: [ClientsService, ClientsGateway],
